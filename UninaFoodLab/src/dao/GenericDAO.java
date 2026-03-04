d6182dd62d81a28dc4824efacdd8006d0cc711e1
gigli <gigliosimona15@gmail.com>
2026-03-04 16:26:41 +0100

    Creazione classe generica per DAO

---
+++ UninaFoodLab/src/dao/GenericDAO.java
@@ -0,0 +1,17 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import database.DBConnection;
+
+public abstract class GenericDAO {
+    
+	protected Connection getConnection() throws SQLException {
+        return DBConnection.getConnection();
+    }
+    
+    public abstract void create(Object o) throws SQLException;
+    public abstract Object read(int id) throws SQLException;
+    public abstract void update(Object o) throws SQLException;
+    public abstract void delete(int id) throws SQLException;
+}
